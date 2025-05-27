80bfdb07495cf12655c323832c013cc6a6cc4d77
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2025-05-27 22:46:13 +0300

    got a rest API up and running

---
+++ xperience/src/main/java/com/moecodes/xperience/controller/TaskController.java
@@ -0,0 +1,15 @@
+package com.moecodes.xperience.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    @GetMapping
+    public String getHello() {
+        return "Hello Babe!";
+    }
+}
