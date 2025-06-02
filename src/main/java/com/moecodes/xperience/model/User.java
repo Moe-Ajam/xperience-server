078b126b7bb79b82d6cd78f3c4d49932b59a02b3
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2025-06-02 22:45:15 +0300

    Fixed the generation strategy to be AUTO to give the responsibility to the database

--- src/main/java/com/moecodes/xperience/model/User.java
+++ src/main/java/com/moecodes/xperience/model/User.java
@@ -9,6 +9,7 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -22,7 +23,7 @@ import lombok.NoArgsConstructor;
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
     @Column(nullable = false, unique = true, length = 50)
