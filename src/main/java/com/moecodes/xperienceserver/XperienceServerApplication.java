79113c10dad6a0a652d0c33f2b6f9955f2e32c0c
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2025-06-21 21:03:31 +0300

    Initial Commit from Intellij

---
+++ src/main/java/com/moecodes/xperienceserver/XperienceServerApplication.java
@@ -0,0 +1,13 @@
+package com.moecodes.xperienceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XperienceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XperienceServerApplication.class, args);
+    }
+
+}
