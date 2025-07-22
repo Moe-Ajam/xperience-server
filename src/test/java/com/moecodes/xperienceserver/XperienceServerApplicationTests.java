71b76e6f39e08e971be0ee41b9e10a86c74bccb7
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2025-07-22 19:47:13 +0300

    Initial commit after resetting with clean Spring Security config

---
+++ src/test/java/com/moecodes/xperienceserver/XperienceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moecodes.xperienceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XperienceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
